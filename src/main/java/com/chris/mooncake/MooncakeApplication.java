f407be0081d651c4acd23bd715491b31496d281b
chrislin2021 <chris.lin0131@gmail.com>
2022-06-04 19:12:41 +0800

    初始化程式環境

---
+++ src/main/java/com/chris/mooncake/MooncakeApplication.java
@@ -0,0 +1,13 @@
+package com.chris.mooncake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MooncakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MooncakeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chris/mooncake/MooncakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.mooncake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MooncakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
